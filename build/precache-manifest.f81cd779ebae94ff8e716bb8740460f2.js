debeceda4f64307d3c5ec150a937d6c1c55e6584
harsh.sahay <harsh.sahay@rodio.in>
2020-01-06 14:30:49 +0530

    first commit

---
+++ build/precache-manifest.f81cd779ebae94ff8e716bb8740460f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d274383fba0024b8c04f1af74a4c24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbe289d0187f80aac7cc",
+    "url": "/static/css/main.ac470cf0.chunk.css"
+  },
+  {
+    "revision": "6eb35a67b99f08a72db2",
+    "url": "/static/js/2.bb85ca39.chunk.js"
+  },
+  {
+    "revision": "e5fdd3c10a39c54f9723efcb1214ee6a",
+    "url": "/static/js/2.bb85ca39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbe289d0187f80aac7cc",
+    "url": "/static/js/main.3424d252.chunk.js"
+  },
+  {
+    "revision": "5536b1ffa5673e4eb964",
+    "url": "/static/js/runtime-main.d07f956b.js"
+  }
+]);
